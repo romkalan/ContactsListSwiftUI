fb3ebaed56834a78b2bf91399d4325f972998d25
romkalan <romkalan@mail.ru>
2023-05-04 22:00:31 +0300

    correction detailInformationView

--- ContactsListSwiftUI/Views/DetailInfoView.swift
+++ ContactsListSwiftUI/Views/DetailInfoView.swift
@@ -11,28 +11,47 @@ struct DetailInfoView: View {
     let person: Person
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text(person.fullName)
-                .font(.largeTitle)
-            Image(systemName: "person.fill")
-                .resizable()
-                .frame(width: 100, height: 100)
-            HStack {
-                Image(systemName: Contacts.phone.rawValue)
-                Text(person.phoneNumber)
+        NavigationStack {
+            List {
+                HStack {
+                    Spacer()
+                    Image(systemName: "person.fill")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                    Spacer()
+                }
+                PersonalContactView(
+                    imageName: "phone",
+                    color: .blue,
+                    communication: person.phoneNumber
+                )
+                PersonalContactView(
+                    imageName: "tray",
+                    color: .blue,
+                    communication: person.email
+                )
             }
-            HStack {
-                Image(systemName: Contacts.email.rawValue)
-                Text(person.email)
-            }
+            .navigationTitle(person.fullName)
         }
-        .listStyle(.plain)
-        .padding()
     }
 }
 
 struct DetailInfoView_Previews: PreviewProvider {
     static var previews: some View {
         DetailInfoView(person: Person(id: 1, name: "Albert", surname: "Johnons", email: "ttt@mail.ru", phoneNumber: "4221"))
+    }
+}
+
+struct PersonalContactView: View {
+    let imageName: String
+    let color: Color
+    let communication: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: imageName)
+                .foregroundColor(color)
+            Text(communication)
+        }
     }
 }
