803706a77ce1e3a6bace24ad8d1a3eb0a4fcac34
romkalan <romkalan@mail.ru>
2023-05-04 19:37:16 +0300

    add model and dataStore

---
+++ ContactsListSwiftUI/Models/Person.swift
@@ -0,0 +1,56 @@
+//
+//  Person.swift
+//  ContactsListSwiftUI
+//
+//  Created by Roman Lantsov on 04.05.2023.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let surname: String
+    let email: String
+    let phoneNumber: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+}
+
+extension Person {
+    static func getContactList() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        let names = DataStore.shared.names.shuffled()
+        let surnames = DataStore.shared.surnames.shuffled()
+        let emails = DataStore.shared.emails.shuffled()
+        let phones = DataStore.shared.phones.shuffled()
+        
+        let iterationCount = min(
+            names.count,
+            surnames.count,
+            emails.count,
+            phones.count
+        )
+        
+        for index in 0..<iterationCount {
+            let person = Person(
+                name: names[index],
+                surname: surnames[index],
+                email: emails[index],
+                phoneNumber: phones[index]
+            )
+            
+            persons.append(person)
+        }
+        
+        return persons
+    }
+}
+
+enum Contacts: String {
+    case phone = "phone"
+    case email = "tray"
+}
