cde998f6e883e58a8be0c87a2cb4dd12437dda30
romkalan <romkalan@mail.ru>
2023-05-04 19:32:02 +0300

    Initial Commit

---
+++ ContactsListSwiftUI/ContactsListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsListSwiftUIApp.swift
+//  ContactsListSwiftUI
+//
+//  Created by Roman Lantsov on 04.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactsListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
